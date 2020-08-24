98d9625b29ec3f93bf74c7bf95a38c919a2cdb34
jeremy wambugu <wambugujeremy@gmail.com>
2020-08-24 18:17:09 +0300

    added a responsive hamburger menu

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-const hambuger = document.getElementById('hamburger')
-const navUl = document.getElementById('nav-ul')
-hambuger.addEventListener('click', () => {
+const hamburger = document.getElementById('hamburger')
+const navUl = document.getElementById("nav-ul")
+
+hamburger.addEventListener('click', () => {
   navUl.classList.toggle('show')
-
-
 })
-
-// gsap.from('.navigation', { duration: 1, y: "-100%", ease: "power2" });
-// gsap.from('.hero', { duration: 2, y: "-20%", ease: "power2", stagger: 0.1 })
